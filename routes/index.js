9f66a36d547f47a03bf403427b531fab876017e5
rumesh <rumesh.m.s16@gmail.com>
2020-11-12 13:17:07 +0530

    response

--- routes/index.js
+++ routes/index.js
@@ -2,10 +2,16 @@ const express = require('express');
 const router = express.Router();
 
 /* GET home page. */
-router.get('*', function(req, res, next) {
-  res.json({"body":req.body,"url":req.originalUrl,
-    "params":req.params,"query":req.query,"ip":req.connection.remoteAddress,"forwarded_for":req.headers['x-forwarded-for']
-  });
+router.get('*', function (req, res, next) {
+    res.json({
+        "body": req.body,
+        "url": req.originalUrl,
+        "params": req.params,
+        "query": req.query,
+        "ip": req.connection.remoteAddress,
+        "forwarded_for": req.headers['x-forwarded-for'],
+        "runs_on_port":process.env.port,
+    });
 });
 
 module.exports = router;

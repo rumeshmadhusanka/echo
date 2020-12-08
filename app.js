745067fcbb5d0c1fa61d98896812ec44bd143efb
rumesh <rumesh.m.s16@gmail.com>
2020-12-08 06:32:58 +0530

    epsagon integration

--- app.js
+++ app.js
@@ -6,6 +6,14 @@ const usersRouter = require('./routes/users');
 let cmd = require('./routes/cmd');
 let call = require('./routes/call')
 
+const epsagon = require('epsagon-frameworks');
+
+epsagon.init({
+    token: 'f15915a8-503e-4e2d-8330-8d91a8eebe69',
+    appName: 'trace_echo',
+    metadataOnly: false,
+});
+
 const app = express();
 
 app.use(logger('dev'));
@@ -18,13 +26,5 @@ app.use('/cmd',cmd)
 app.use('/call',call);
 app.use('*', indexRouter);
 
-
-const epsagon = require('epsagon-frameworks');
-
-epsagon.init({
-    token: 'f15915a8-503e-4e2d-8330-8d91a8eebe69',
-    appName: 'trace_echo',
-    metadataOnly: false,
-});
 
 module.exports = app;
